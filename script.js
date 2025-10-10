6e5ed994074f2633db213635f76a98854b915460
manojmanoharan2005-dot <manojmahoaran2005@gmail.com>
2025-10-10 15:26:25 +0530

    even  commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let o1=90;
+let o2=90;
+if(o1==o2){
+    console.log("equal");
+}    
+else{
+    console.log("not equal");
+}  
+
---
+++ script2.js
@@ -0,0 +1,7 @@
+function sum(a,b){
+    console.log(a+b);
+}
+const sum2=   (a,b,c)=> {
+    console.log(a+b+c);
+}
+sum2(20,30,60);
