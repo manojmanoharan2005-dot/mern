33acfc73071eb0c6f50783e8f3efbd68af5ab703
manojmanoharan2005-dot <manojmahoaran2005@gmail.com>
2025-10-13 09:51:05 +0530

    pt commit

--- oddarr.js
+++ oddarr.js
@@ -3,3 +3,22 @@ function oddeven(arr){
 }   
 let arr=[1,2,3,4,5,6,7,8,9];
 console.log(oddeven(arr));
+
+
+let array1=[1,2,3,4,5,6];
+let sum=array1.reduce((product,current)=>{
+    return product*current;
+},1)
+console.log(sum);
+
+let array2=[1,2,3,4,5,6];
+let sum1=array2.reduce((sum,current)=>{
+    return sum+current;
+},1)
+console.log(sum1);
+
+let arr1=[1,2,3,4,5,6,7,8,9];
+let evenarr=arr1.filter(num=>num%2==0);
+console.log(evenarr);
+let oddarr=arr1.filter(num=>num%2!=0);
+console.log(oddarr);
