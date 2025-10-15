13488dafcc1ec7ea1947f29500e6a32a1e2eb6ff
manojmanoharan2005-dot <manojmahoaran2005@gmail.com>
2025-10-15 10:34:48 +0530

    o commit

--- script2.js
+++ script2.js
@@ -5,3 +5,29 @@ const sum2=   (a,b,c)=> {
     console.log(a+b+c);
 }
 sum2(20,30,60);
+
+
+
+const mul=(a,b)=>{
+    return a*b;
+}
+const squarwe=(n)=>{
+    return mul(n,n);
+}
+
+const print=(n)=>{
+    let s=squarwe(n);
+    console.log(s)
+};
+print(5);
+
+
+
+
+console =  log('first');
+Promise.resolve().then(console.log('From Promise'));
+setTimeout(() => {
+    console.log('Inside setTimeout');
+},2000);
+
+console.log('Last line of script');
